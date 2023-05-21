431d9b770dcc513059a97119e3f07a8ac02facea
marcelo magalhaes <magawebtec@gmail.com>
2023-05-20 22:48:16 -0300

    index, about

--- src/components/Doubleside.jsx
+++ src/components/Doubleside.jsx
@@ -1,22 +1,17 @@
 export default ({type, title, img, children}) => {
   return (
-    <section class="_front-product mt-5">
-    <div class="_container-fluid p-0">
+    <section class="_front-product ">
+    <div class="_container-fluid _p-4 col-12">
         <div class="row _align-items-center">
             {type==1&&
                 <>
                     <div class="col-lg-6 col-12 align-text-top" style="vertical-align: top;">
-                        <img src={img} class="top img-fluid mb-4" alt=""/>
+                        <img src={img} class="border-4 rounded-5 img-fluid my-4" alt=""/>
                     </div>
-
                     <div class="col-lg-6 col-12">
-                        <div class="px-5 py-5 py-lg-0">
-                            
-                            <h2 class="mb-4">{title}</h2>
-
-                            <p class="lead mb-4">{children}</p>
-
-                        
+                        <div class="_px-5 py-5 py-lg-0">
+                            <h2 class="mb-1">{title}</h2>
+                            <p class="lead mb-4 fs-5 text-dark fw-semibold">{children}</p>
                         </div>
                     </div>
                 </>
@@ -28,7 +23,7 @@ export default ({type, title, img, children}) => {
                             
                             <h2 class="mb-4">{title}</h2>
 
-                            <p class="lead mb-4">{children}</p>
+                            <p class="lead mb-4 fs-5">{children}</p>
 
                         
                         </div>
@@ -55,6 +50,48 @@ export default ({type, title, img, children}) => {
                     </div>
                 </>
             }
+
+            {type==4&&
+               
+             <div class="row">
+                <div class="col-6">
+                    <img src={img} alt="Snow" class=" img-fluid mt-5 shadow-lg"/>
+                </div>
+                
+                <div class="col-6 px-4">
+                {/* <img src={img} alt="Snow" class=" img-fluid mt-4 shadow-lg"/> */}
+                    <h2 class="mb-1">{title}</h2>
+                    <p class="_lead mb-4 text-secondary">{children}</p>
+                </div>
+            </div>
+            }
+            {type==5&&
+               
+               <div class="row">
+                  
+                  <div class="col-6 _px-0">
+                      <h3 class="mb-1">{title}</h3>
+                      <p class="_lead mb-4 fs-5">{children}</p>
+                  </div>
+                  <div class="col-6 ">
+                      <img src={img} alt="Snow" class=" img-fluid mt-4"/>
+                  </div>
+              </div>
+              }
+            {type==6&&
+               
+               <div class="row">
+                 
+                  
+                  <div class="col-12 px-4">
+                  <div class="text-center m-4">
+				<h2>{title}</h2>
+			</div>
+                      <p class="_lead mb-4">{children}</p>
+                  </div>
+                 
+              </div>
+              }
         </div>
     </div>
 </section>
