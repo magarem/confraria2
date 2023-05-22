8da6280feb0e69afb6b95f0671ba9402555df0a2
marcelo magalhaes <magawebtec@gmail.com>
2023-05-21 23:06:49 -0300

    npm i2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 // import tailwind from "@astrojs/tailwind";
 // import vue from '@astrojs/vue';
 
-import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
